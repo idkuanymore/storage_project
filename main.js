762f154f62633259d06d92ad307c06d5c3487428
layer <b1lder_yt@mail.ru>
2025-05-13 20:27:31 +0300

    func_to_buttons

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-document.querySelector('.add_temp').addEventListener('click', function() {
-  ///
+var CURRENT_MIN = 0
+var CURRENT_MAX = 0
+
+let max_temp = document.querySelector('.max_temp')
+  max_temp.innerHTML = CURRENT_MAX
+
+  let min_temp = document.querySelector('.min_temp')
+    min_temp.innerHTML = CURRENT_MIN
+
+
+document.querySelector('#increase-temperature-max').addEventListener('click', function() {
+    CURRENT_MAX+=1
+    max_temp.innerHTML = CURRENT_MAX
 })
 
-document.querySelector('.rem_temp').addEventListener('click', function() {
-  ///
+document.querySelector('#decrease-temperature-max').addEventListener('click', function() {
+    CURRENT_MAX-=1
+    max_temp.innerHTML = CURRENT_MAX
 })
 
+document.querySelector('#increase-temperature-min').addEventListener('click', function() {
+    CURRENT_MIN+=1
+    min_temp.innerHTML = CURRENT_MIN
+})
 
+document.querySelector('#decrease-temperature-min').addEventListener('click', function() {
+    CURRENT_MIN-=1
+    min_temp.innerHTML = CURRENT_MIN
+})
+
+// асинхронная функция крч
